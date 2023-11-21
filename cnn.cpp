952de193c72845a1fedc42d2633d0b4948f3659f
Stéphane <26119556+S12P@users.noreply.github.com>
2023-11-20 22:06:48 -0800

    Create cnn.cpp

---
+++ cnn.cpp
@@ -0,0 +1,60 @@
+const int kNum = 256;
+const int kKernel = 5;
+const int kImSize = 224;
+const int kInImSize = 228;
+const int kOutImSize = 112;
+
+
+#define max(X,Y) ((X)>(Y)?(X):(Y))
+
+#pragma ACCEL kernel
+void Cnn(
+    const float input[kNum][kInImSize][kInImSize],
+    const float weight[kNum][kNum][kKernel][kKernel], const float bias[kNum],
+    float output[kNum][kOutImSize][kOutImSize]) {
+
+  static float C[kNum][kImSize][kImSize];
+
+  for (int i = 0; i < kNum; ++i) {
+    for (int h = 0; h < kImSize; ++h) {
+      for (int w = 0; w < kImSize; ++w)
+        C[i][h][w] = bias[i];
+    }
+  }
+
+  // Convolution
+  for (int i = 0; i < kNum; ++i) {
+    for (int j = 0; j < kNum; ++j) {
+      for (int h = 0; h < kImSize; ++h) {
+        for (int w = 0; w < kImSize; ++w) {
+          for (int p = 0; p < kKernel; ++p) {
+            for (int q = 0; q < kKernel; ++q)
+              C[i][h][w] += weight[i][j][p][q] * input[j][h + p][w + q];
+          }
+        }
+      }
+    }
+  }
+
+  // ReLU
+  for (int i = 0; i < kNum; ++i) {
+    for (int h = 0; h < kImSize; ++h) {
+      for (int w = 0; w < kImSize; ++w) {
+        C[i][h][w] = max(0.f, C[i][h][w]);
+      }
+    }
+  }
+
+  // Max pooling
+  for (int i = 0; i < kNum; ++i) {
+    for (int h = 0; h < kOutImSize; ++h) {
+      for (int w = 0; w < kOutImSize; ++w) {
+        output[i][h][w] = max(
+            max(C[i][h * 2][w * 2    ], C[i][h * 2 + 1][w * 2    ]),
+            max(C[i][h * 2][w * 2 + 1], C[i][h * 2 + 1][w * 2 + 1]));
+      }
+    }
+  }
+}
+
+
